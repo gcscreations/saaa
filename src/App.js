8732438425653a8cef5489ca3001aa10c3c93de6
gcscreations <gcscreations.ar.game@gmail.com>
2023-09-15 16:10:56 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import MindARThreeViewer from './mindar-three-viewer';
 
 function App() {
   const [started, setStarted] = useState(null);
+    ///const [started, setStarted] = useState("aframe");
 
   return (
     <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+        <div className="container">
+            <MindARThreeViewer/>
+
+        </div>
+        {/*<div className="control-buttons">
+            {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+
+            {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
+        </div>
+
+        {started === 'aframe' && (
+            <div className="container">
+                <MindARViewer/>
+                <video></video>
+            </div>
+        )}*/}
+
+
+
+        {/*<h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
 
       <div className="control-buttons">
         {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
@@ -27,7 +47,7 @@ function App() {
         <div className="container">
           <MindARThreeViewer />
         </div>
-      )}
+      )}*/}
     </div>
   );
 }
--- src/mindar-three-viewer.js
+++ src/mindar-three-viewer.js
@@ -8,14 +8,40 @@ export default () => {
   useEffect(() => {
     const mindarThree = new MindARThree({
       container: containerRef.current,
-      imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind"
+      imageTargetSrc: "../data/targets.mind",
+      filterMinCF: 1,
+      filterBeta: 10000,
+      missTolerance: 0,
+      warmupTolerance: 0
     });
-    const {renderer, scene, camera} = mindarThree;
+    function Box()
+    {
+      const anchor1 = mindarThree.addAnchor(1);
+      const geometry1 = new THREE.PlaneGeometry(1, 1);
+      const material1 = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+      const plane1 = new THREE.Mesh( geometry1, material1 );
+      anchor1.group.add(plane1);
+    }
+    const {renderer, scene, camera,container} = mindarThree;
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+    // update the camera's frustum
+    camera.updateProjectionMatrix();
+
+    // update the size of the renderer AND the canvas
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    // set the pixel ratio (for mobile devices)
+    renderer.setPixelRatio(window.devicePixelRatio);
     const anchor = mindarThree.addAnchor(0);
-    const geometry = new THREE.PlaneGeometry(1, 0.55);
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+
+    const geometry = new THREE.PlaneGeometry(1, 0.65);
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: false, opacity: 0.5} );
     const plane = new THREE.Mesh( geometry, material );
     anchor.group.add(plane);
+    Box();
+
+
 
     mindarThree.start();
     renderer.setAnimationLoop(() => {
